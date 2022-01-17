d6ae4260b0110d3865fec4dbc728ea83e8a0604e
yangningtao <yangningtao21@mails.ucas.ac.cn>
2022-01-17 23:50:54 +0800

    first commit

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        int x = 0;
+        while (x < 10) {
+            System.out.println(x);
+            x = x + 1;
+        }
+    }
+}
